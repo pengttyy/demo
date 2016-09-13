7fce523b78c0967873e0d7cd188c1cf85a11c99c
pengttyy <pengttyy@gmail.com>
2016-09-13 23:52:15 +0800

    submit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by KaiPeng on 2016-09-13,0013.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(System.getenv("WAS_HOME"));
+
+    }
+}
